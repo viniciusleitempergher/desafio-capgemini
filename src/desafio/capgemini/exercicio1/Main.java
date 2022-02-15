8dcf72605f9c24c1e9e6ef11b725b2d45dc8509e
Vinícius Gabriel Leitempergher <viniciusgabriel.com@gmail.com>
2022-02-15 00:35:11 -0300

    exercício 2 feito

--- src/desafio/capgemini/exercicio1/Main.java
+++ src/desafio/capgemini/exercicio1/Main.java
@@ -3,18 +3,20 @@ package desafio.capgemini.exercicio1;
 public class Main {
 	public static void main(String[] args) {
 		// Tamanho da escada
-		int n = 6;
+		int n = 15;
 
-		// Possuirá a escada desenhada em caractéres
+		// Possuirá a escada desenhada em caracteres
 		String escada = "";
 	
 		
 		for (int i = 1; i <= n; i++) {
 			
+			// Repete pela quantidade de vezes do tamanho da escada menos a quantidade de asteriscos da linha atual
 			for (int l = 0; l <= n - i; l++) {
 				escada += " ";
 			}
 			
+			// Repete pela quantidade de asteriscos da linha atual
 			for (int l = 0; l < i; l++) {
 				escada += "*";
 			}
--- src/desafio/capgemini/exercicio2/Main.java
+++ src/desafio/capgemini/exercicio2/Main.java
@@ -1,10 +1,23 @@
 package desafio.capgemini.exercicio2;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite a senha: ");
+		String senha = sc.next();
+		
+		if (senha.length() < 6) {
+			// Número de caracteres que faltam para a senha possuir 6
+			int caracteresFaltando = 6 - senha.length();
+			
+			System.out.println("A senha não possui o mínimo de caracteres, faltam " + caracteresFaltando + "!");
+		} else {
+			System.out.println("Sua senha possui o mínimo de caracteres!");
+		}
 	}
 
 }
